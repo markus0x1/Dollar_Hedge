379c5bfca67cca798dc4680f2215cbd9511183c9
SchickMarkus <33206087+SchickMarkus@users.noreply.github.com>
2020-10-10 20:34:20 +0200

    some preliminary EUR/USD analysis
    code for preliminary analysis.
    delete missing values from csv by hand, cause python/pandas is a b*tch

--- EUR_USD/euro_usd_analysis.py
+++ EUR_USD/euro_usd_analysis.py
@@ -1,15 +1,18 @@
 import pandas as pd
-
+import seaborn as sns
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('TkAgg')
 
 if __name__ == "__main__":
     df = pd.read_csv("DEXUSEU.csv",  parse_dates=['DATE'])
 
     """ some cleaning"""
-    df = df.rename(columns = {"DATE": "date", "DEXUSEU": "value"})
-    is_missing = df["value"]=="."
-    df.loc[is_missing, "value"] == NaN
-
+    df = df.rename(columns = {"DATE": "date", "DEXUSEU": "EUR/USD"})
+    sns.lineplot(data=df, x="date", y="EUR/USD")
+    plt.show()
     print("hello world")
+
 
 
 
