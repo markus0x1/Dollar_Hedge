b5a92a0de2f58fe7c72c5940fc9975315b2ad5f1
SchickMarkus <33206087+SchickMarkus@users.noreply.github.com>
2020-10-21 22:48:37 +0200

    shorten long-name erc20

--- contracts/aEURs.sol
+++ contracts/aEURs.sol
@@ -5,5 +5,5 @@ pragma solidity ^0.6.2;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.0.1/contracts/presets/ERC20PresetMinterPauser.sol";
 
 contract aEURs is ERC20PresetMinterPauser {
-    constructor() public ERC20PresetMinterPauser("aEURs", "Aave Euro stable") {}
+    constructor() public ERC20PresetMinterPauser("aEURs", "aEuro stable") {}
 }
